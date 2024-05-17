be8113b1be643ea98b066da32575251bb0f8ec57
hemanshu-faldu <hemanshu.faldu@pirimidtech.com>
2024-05-17 21:58:49 +0530

    env added

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from random_word import RandomWords
+import environ
+env = environ.Env()
+
 r = RandomWords()
 # Create your views here.
 
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(r.get_random_word())
+    return HttpResponse("Name :- "+env('myName') + "  "+r.get_random_word())
